bff8173d6f8dc916b2f6c43cb5b426425c71a88f
Nigelbueno <nigelbuenodemesquita@iMac-van-Nigel-Bueno-de-Mesquita.local>
2015-06-24 18:47:56 +0200

    Added list to settings of app, and did some other stuff..

--- SlowChat/ViewController.swift
+++ SlowChat/ViewController.swift
@@ -7,7 +7,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         // Todo: Add checking the credentials and stuff
         
-        showAlertIfNotLoggedIn()
+        var isAutoLoginEnabled = NSUserDefaults.standardUserDefaults().boolForKey("auto_login_pref")
+        
+        if isAutoLoginEnabled {
+            if login() {
+                // Show the categories
+            } else {
+                showCredentialsIncorrectAlert()
+            }
+        } else {
+            showAutoLoginDisabledAlert()
+        }
         
         NSNotificationCenter.defaultCenter().addObserver(
             self,
@@ -22,25 +32,28 @@ class ViewController: UIViewController {
     }
     
     func defaultsChanged(notification : NSNotification) {
-        showAlertIfNotLoggedIn()
+        showAutoLoginDisabledAlert()
     }
     
-    private func showAlertIfNotLoggedIn() {
-        var isLoggedIn = NSUserDefaults.standardUserDefaults().boolForKey("auto_login_pref")
-        
-        if !isLoggedIn {
-            var alert = UIAlertController(title: "You're not logged in!", message: "Automatic login is not enabled. Do you wish to login manually now?", preferredStyle: UIAlertControllerStyle.Alert)
+    private func showAutoLoginDisabledAlert() {
+        var alert = UIAlertController(title: "Auto login is disabled", message: "Automatic login is not enabled. Do you wish to login manually now?", preferredStyle: UIAlertControllerStyle.Alert)
             
-            alert.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { (action) -> Void in
+        alert.addAction(UIAlertAction(title: "Yes", style: .Default, handler: { (action) -> Void in
                 
-            }))
+        }))
             
-            alert.addAction(UIAlertAction(title: "No", style: .Default, handler: nil))
+        alert.addAction(UIAlertAction(title: "No", style: .Default, handler: nil))
             
-            self.presentViewController(alert, animated: true, completion: nil)
-        }
+        self.presentViewController(alert, animated: true, completion: nil)
     }
-
+    
+    private func showCredentialsIncorrectAlert() {
+        
+    }
+    
+    private func login() -> Bool {
+        return true;
+    }
 
 }
 
